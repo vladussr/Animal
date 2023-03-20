29d0d2882b2380beba9733acf60ea7af0a8bf13f
Vlad <vladussr@mail.ru>
2023-03-20 14:09:13 +0800

    Выполнил второе задание

--- src/Animal.java
+++ src/Animal.java
@@ -3,13 +3,35 @@ public class Animal {
     int age;
     double weigth;
     boolean isFly, isWalk, isSwim;
+
+    public Animal(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public Animal(String type, int age) {
+        this.type = type;
+        this.name = "No Name";
+        this.age = age;
+    }
+
+    public Animal(String type, String name, int age, double weigth, boolean isFly, boolean isWalk, boolean isSwim) {
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.weigth = weigth;
+        this.isFly = isFly;
+        this.isWalk = isWalk;
+        this.isSwim = isSwim;
+    }
+
     void display(){
         System.out.println("Тип животного: " + type + ", Имя: " + name + ", Возраст: " + age + ", Вес: " + weigth + ", Умеет летать: " + (isFly?"Да":"Нет") + ", Умеет ходить: " + (isWalk?"Да":"Нет") + ", Умеет плавать: " + (isSwim?"Да":"Нет"));
     }
      void rename(String name){
         this.name = name;
      }
-     void holiday(int day){
-        this.weigth += 0.1*day;
+     void holiday(int day) {
+         this.weigth += 0.1 * day;
      }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,23 @@
-public class Main {
+class Main{
     public static void main(String[] args) {
-        Animal tiger = new Animal();
-        tiger.type = "тигр";
-        tiger.name = "Артём";
+        Animal tiger = new Animal("тигр", "Артём");
         tiger.age = 15;
         tiger.weigth = 300.6;
         tiger.isSwim = true;
         tiger.isWalk = true;
         tiger.isFly = false;
         tiger.display();
-        tiger.rename("Антон");
-        tiger.display();
 
-        Animal sparrow = new Animal();
-        sparrow.type = "воробей";
-        sparrow.name = "Капитан Джек";
-        sparrow.age = 3;
+        Animal sparrow = new Animal("воробей", 5);
         sparrow.weigth = 2;
         sparrow.isSwim = false;
         sparrow.isWalk = true;
         sparrow.isFly = true;
         sparrow.display();
-        sparrow.holiday(2);
-        sparrow.display();
+
+        Animal duck = new Animal("Утка", "Утя", 3, 5.8, true, true, true);
+        duck.holiday(5);
+        duck.rename("Утя-Утя");
+        duck.display();
     }
 }
